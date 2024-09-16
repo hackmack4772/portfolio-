cbd063a711da43db547b8a59ab9da9829d7fe361
hackmack4772 <aamir.saleem@shinedezign.com>
2024-09-16 11:48:27 +0530

    First commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,114 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.jpeg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple">INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              Hello! I'm <span className="purple"> Aamir Saleem Lone</span>,
+              a passionate developer with a love for programming. I have gained
+              valuable experience and learned a lot along the way… 🤷‍♂️
+              <br />
+              <br />I'm proficient in core languages like
+              <i>
+                <b className="purple"> C++, JavaScript, and Go.</b>
+              </i>
+              <br />
+              <br />
+              My interests lie in crafting innovative &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products</b>
+              </i>
+              , as well as exploring areas related to{" "}
+              <b className="purple">Blockchain.</b>
+              <br />
+              <br />
+              I am also passionate about developing with <b className="purple">Node.js</b> and
+              <i>
+                <b className="purple">
+                  {" "}
+                  Modern JavaScript Libraries and Frameworks
+                </b>
+              </i>
+              &nbsp; such as
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect</span> with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/hackmack4772"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/CRew4772"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/aamir-saleem-lone/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/aamir-saleem-lone"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Home2;
