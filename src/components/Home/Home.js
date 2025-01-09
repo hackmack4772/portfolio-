5e979db3cea8716d15a4048f97a16de9cc335885
hackmack4772 <aamir.saleem@shinedezign.com>
2025-01-09 09:58:19 +0530

    contact us form

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import ContactForm from "./ContactForm";
 
 function Home() {
   return (
@@ -29,15 +30,17 @@ function Home() {
                 <Type />
               </div>
             </Col>
-{/* 
+
             <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
+            
+            <ContactForm/>
+              {/* <img
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
                 style={{ maxHeight: "450px" }}
-              />
-            </Col> */}
+              /> */}
+            </Col>
           </Row>
         </Container>
       </Container>
