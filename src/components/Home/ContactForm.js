d743ad1dc45269fabadc352cfac9b1d9cf67bfa3
hackmack4772 <aamir.saleem@shinedezign.com>
2025-01-10 09:46:39 +0530

    contact form changes

--- src/components/Home/ContactForm.js
+++ src/components/Home/ContactForm.js
@@ -1,83 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactUs.css'; // Assuming you create a separate CSS file for styles
 
 const ContactUs = () => {
-  return (
-    <div className="outer">
-      <div className="main" style={{ marginTop: '100px' }}>
-        <section className="contact_us">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-8">
-                <div className="contact_inner">
-                  <div className="row">
-                    <div className="col-lg-8">
-                      <div className="contact_form_inner">
-                        <div className="contact_field">
-                          <h3>Contact Us</h3>
-                          <p>Feel Free to contact us any time. We will get back to you as soon as we can!</p>
-                          <input type="text" className="form-control form-group" placeholder="Name" />
-                          <input type="text" className="form-control form-group" placeholder="Email" />
-                          <textarea className="form-control form-group" placeholder="Message"></textarea>
-                          <button className="contact_form_submit">Send</button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="col-lg-4">
-                      <div className="right_contact_social_icon d-flex align-items-end">
-                        <div className="social_item_inner d-flex">
-                          <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
-                          <li><a href="#"><i className="fab fa-instagram"></i></a></li>
-                          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="contact_info_sec">
-                    <h4>Contact Info</h4>
-                    <div className="d-flex info_single align-items-center">
-                      <i className="fas fa-headset"></i>
-                      <span>+91 9596581274</span>
-                    </div>
-                    <div className="d-flex info_single align-items-center">
-                      <i className="fas fa-envelope-open-text"></i>
-                      <span>hackmackcrew4772@gmail.com</span>
-                    </div>
-                    <div className="d-flex info_single align-items-center">
-                      <i className="fas fa-map-marked-alt"></i>
-                      <span>Handwara jammu and kashmir ,193302</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
-        <section className="map_sec">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-10">
-                <div className="map_inner">
-                  <h4>Find Us on Google Map</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quo beatae quasi assumenda, expedita aliquam minima tenetur maiores neque incidunt repellat aut voluptas hic dolorem sequi ab porro, quia error.</p>
-                  <div className="map_bind">
-                    <iframe
-                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471220.5631094339!2d88.04952462217592!3d22.6757520733225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1596988408134!5m2!1sen!2sin"
-                      width="100%"
-                      height="450"
-                      frameBorder="0"
-                      style={{ border: 0 }}
-                      allowFullScreen=""
-                      aria-hidden="false"
-                      tabIndex="0"
-                    ></iframe>
-                  </div>
-                </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents default form submission behavior
+    console.log('Form Data:', formData); // You can replace this with an API call or another action
+    alert(`Thank you, ${formData.name}! Your message has been sent.`);
+    
+    // Optionally reset the form after submission
+    setFormData({ name: '', email: '', message: '' });
+  };
+
+  return (
+    <div className="contact_inner">
+      <div className="slogan">Connecting People, Building Dreams</div>
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="contact_form_inner">
+            <form onSubmit={handleSubmit}>
+              <div className="contact_field">
+                <h3>Contact Us</h3>
+                <p>Feel Free to contact us any time. We will get back to you as soon as we can!</p>
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control form-group"
+                  placeholder="Name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control form-group"
+                  placeholder="Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                <textarea
+                  name="message"
+                  className="form-control form-group"
+                  placeholder="Message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+                <button type="submit" className="contact_form_submit">
+                  Send
+                </button>
               </div>
+            </form>
+          </div>
+        </div>
+        {/* <div className="col-lg-4">
+          <div className="right_contact_social_icon d-flex align-items-end">
+            <div className="social_item_inner d-flex">
+              <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
+              <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+              <li><a href="#"><i className="fab fa-twitter"></i></a></li>
             </div>
           </div>
-        </section>
+        </div> */}
+      </div>
+      <div className="contact_info_sec">
+        <h4>Contact Info</h4>
+        <div className="d-flex info_single align-items-center">
+          <i className="fas fa-headset"></i>
+          <span>+91 9596581274</span>
+        </div>
+        <div className="d-flex info_single align-items-center">
+          <i className="fas fa-envelope-open-text"></i>
+          <span>hackmackcrew4772@gmail.com</span>
+        </div>
+        <div className="d-flex info_single align-items-center">
+          <i className="fas fa-map-marked-alt"></i>
+          <span>Handwara jammu and kashmir ,193302</span>
+        </div>
       </div>
     </div>
   );
