dab3a810456c51da9f8a03f24154cbcfa6b2ea75
Aamir saleem lone <120358827+hackmack4772@users.noreply.github.com>
2025-01-24 04:19:33 +0000

    find the data from the firebase

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,55 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import { collection, getDocs, doc } from "firebase/firestore";
+import { db } from "../../config/firebase";
 import "./education.css";
 
 function Education() {
-  const educationData = [
-    {
-      id: 1,
-      title: "Master of Computer Application (MCA)",
-      institution:
-        "Swami Vivekanand Institute of Engineering & Technology, Rajpura, Punjab",
-      year: "2023 - 2025",
-      score: "Expected Completion: July 2025",
-      image:
-        "https://images.collegedunia.com/public/college_data/images/campusimage/165245050801.00_05_46_42.Still008.jpg",
-    },
-    {
-      id: 2,
-      title: "Bachelors of Computer Application (BCA)",
-      institution: "RIMT University, Mandi Gobindgarh, Punjab",
-      year: "2019 - 2022",
-      score: "CGPA: 9.08",
-      image:
-        "https://content.jdmagicbox.com/comp/fatehgarh-sahib/l6/9999pxxxx.xxxx.100611182107.d1l6/catalogue/rimt-institute-of-engineering-and-technology-mandigobindgarh-fatehgarh-sahib-engineering-colleges-zqg3t7o.jpg",
-    },
-    {
-      id: 3,
-      title: "12th Standard",
-      institution: "Government Higher Secondary School, Handwara",
-      year: "2017 - 2018",
-      score: "Percentage: 80.4%",
-      image:
-        "https://content3.jdmagicbox.com/comp/kupwara/u6/9999p1955.1955.221122221134.x3u6/catalogue/govt-boys-higher-secondary-handwara-kupwara-secondary-schools-9d1de1o7ya.jpg",
-    },
-    {
-      id: 4,
-      title: "10th Standard",
-      institution: "Government High School, Sanzipora",
-      year: "2015 - 2016",
-      score: "Percentage: 86.6%",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKzyc1JASdv-3qUBIaCthlSpXbTCELIilwIA&s",
-    },
-    {
-      id: 5,
-      title: "1st to 8th Standard",
-      institution: "Army Goodwill School, Naugam",
-      year: "2008 - 2015",
-      image:
-        "https://asianlite.uk/wp-content/uploads/2022/02/FKCFVQTaUAAPD55-1024x767.jpg",
-    },
-  ];
+  const [educationData, setEducationData] = useState([]);
+  const fetchEducationDataFromFirestore = async () => {
+    try {
+      const hackmackDocRef = doc(db, "hackmack", "user_education");
+      const educationRef = collection(hackmackDocRef, "educationData");
+      const querySnapshot = await getDocs(educationRef);
+
+      const fetchedData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setEducationData(fetchedData);
+    } catch (error) {
+      console.error("Error fetching education data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEducationDataFromFirestore();
+  }, []);
 
   return (
     <Container fluid className="education-section">
