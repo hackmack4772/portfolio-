72a86007c474b68d86401b4957461872d5191aad
Aamir saleem lone <120358827+hackmack4772@users.noreply.github.com>
2025-01-25 05:10:21 +0000

    name was removed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> </strong>
+                <strong className="main-name"> Aamir Saleem Lone</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
