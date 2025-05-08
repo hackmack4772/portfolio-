56e38633efbe56b7a1c0591aa8ac474e88641aee
Aamir saleem lone <120358827+hackmack4772@users.noreply.github.com>
2025-05-08 10:49:11 +0000

    added the link

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -73,7 +73,7 @@ function About() {
       Kashmiri: "Basic understanding and communication skills"
     },
     hobbies: ["Listening to music", "Learning new technologies", "Photography", "Playing basketball"],
-    image:"public/avatar.jpeg",
+    image:"https://i.postimg.cc/fTG19xq0/1737006453580.jpg",
     contact: {
       location: "San Zipora Kutlari, Kupwara, Jammu and Kashmir, 193302",
       phone: "9596581274",
