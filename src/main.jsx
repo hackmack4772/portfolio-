b4e2e7412a3f07e2d3f1393a532e4bea23f98b63
Aamir saleem lone <120358827+hackmack4772@users.noreply.github.com>
2025-07-14 08:36:25 +0000

    tracking users data

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import './style.css';
 import App from './App';
-// import ActivityTracker from "./components/ActivityTracker";
+import ActivityTracker from "./components/ActivityTracker";
 import { DarkModeProvider } from './Context/DarkModeContext';
 import { LoadingProvider } from './Context/LoadingContext';
 import Particle from "./components/Particle";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <ActivityTracker /> */}
+    <ActivityTracker />
     <LoadingProvider>
       <DarkModeProvider>
         <App />
